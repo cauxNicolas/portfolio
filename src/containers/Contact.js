f220edc998a4a7d5b77ab623e3c33dd063accf3d
cauxNicolas <nicaux95@gmail.com>
2021-01-22 00:52:19 +0100

    bug sticky-menu function input

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+	const [name, setname] = useState("");
+	const [lastname, setlastname] = useState("");
+	const [email, setemail] = useState("");
+
+	const handleChangeName = (event) => {
+		setname(event.target.value);
+	};
+
+	const handleChangeLastname = (event) => {
+		setlastname(event.target.value);
+	};
+
+	const handleChangeEmail = (event) => {
+		setemail(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+
 	return (
 		<>
 			<div id="formulaire" className="effect">
@@ -12,19 +32,34 @@ const Contact = () => {
 					répondrai.
 				</p>
 
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div className="d-flex space-between">
 						<div className="input-flex">
 							<label>Nom</label>
-							<input placeholder="Nom" type="text" />
+							<input
+								placeholder="Nom"
+								type="text"
+								value={name}
+								onChange={handleChangeName}
+							/>
 						</div>
 						<div className="input-flex">
 							<label>Prénom</label>
-							<input placeholder="Prénom" type="text" />
+							<input
+								placeholder="Prénom"
+								type="text"
+								value={lastname}
+								onChange={handleChangeLastname}
+							/>
 						</div>
 					</div>
 					<label>Email</label>
-					<input placeholder="Email" type="text" />
+					<input
+						placeholder="Email"
+						type="email"
+						value={email}
+						onChange={handleChangeEmail}
+					/>
 					<label>Votre projet</label>
 					<textarea
 						rows="5"
