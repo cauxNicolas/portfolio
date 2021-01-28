6bf33f8d96250c14ee931ac272a4073936308349
cauxNicolas <nicaux95@gmail.com>
2021-01-28 21:27:29 +0100

    page works css sub-menu

--- src/containers/Works.js
+++ src/containers/Works.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Works = () => {
-	return <div>Works</div>;
+	return (
+		<div id="works">
+			<div className="content-works">
+				<nav>
+					<div>
+						<a>All</a>
+					</div>
+					<div>
+						<a>Dev</a>
+					</div>
+					<div>
+						<a>Design</a>
+					</div>
+					<div>
+						<a>Branding</a>
+					</div>
+					<div>
+						<a>Video</a>
+					</div>
+				</nav>
+				<div className="preview-works"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Works;
